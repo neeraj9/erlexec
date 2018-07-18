63d2248d8357dd3c8ba04187056639ec8df4e972
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2018-07-18 13:05:26 +0530

    ensure backward compatibility with OTP-20

--- include/exec.hrl
+++ include/exec.hrl
@@ -6,3 +6,12 @@
 -define(SIGUSR2, -12).
 
 -define(FMT(Fmt, Args), lists:flatten(io_lib:format(Fmt, Args))).
+
+-ifdef('FUN_STACKTRACE').
+-define(CAPTURE_STACKTRACE, ).
+-define(GET_STACKTRACE, erlang:get_stacktrace()).
+-else.
+-define(CAPTURE_STACKTRACE, :__StackTrace).
+-define(GET_STACKTRACE, __StackTrace).
+-endif.
+
--- src/exec.erl
+++ src/exec.erl
@@ -665,9 +665,9 @@ init([Options]) ->
         Tab  = ets:new(exec_mon, [protected,named_table]),
         {ok, #state{port=Port, limit_users=Users, debug=Debug, registry=Tab, root=Root}}
     catch
-        _:Reason:StackTrace ->
+        _:Reason ?CAPTURE_STACKTRACE ->
             {stop, ?FMT("Error starting port '~s': ~200p\n  ~s\n",
-                [Exe, Reason, StackTrace])}
+                [Exe, Reason, ?GET_STACKTRACE])}
     end.
 
 %%----------------------------------------------------------------------
